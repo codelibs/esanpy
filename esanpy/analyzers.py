fb4e11541d34da54abe82f67b9d4e357b2da4250
Shinsuke Sugaya <shinsuke@apache.org>
2017-10-11 18:44:25 +0900

    add attributes and converter

--- esanpy/analyzers.py
+++ esanpy/analyzers.py
@@ -18,33 +18,55 @@ except ImportError:
 logger = getLogger('esanpy')
 
 
-def analyzer(text, analyzer='standard', namespace=None,
-             host='localhost', http_port=DEFAULT_HTTP_PORT):
+def default_converter(result):
+    return [x.get('token') for x in result.get('tokens')]
+
+
+def analyzer(text, analyzer='standard', namespace=None, attributes=None,
+             host='localhost', http_port=DEFAULT_HTTP_PORT,
+             converter=None):
     if text is None:
         return []
     data = {'analyzer': analyzer, 'text': text}
+    if attributes is not None:
+        data.update({"explain": True,
+                     "attributes": attributes})
+        if converter is None:
+            converter = lambda x: x
+    elif converter is None:
+        converter=default_converter
     url_suffix = '/_analyze' if namespace is None else '/' + namespace + '/_analyze'
     return send_analyze_request('http://' + host + ':' + str(http_port) + url_suffix,
-                                data)
+                                data,
+                                converter=converter)
 
 
-def custom_analyzer(text, namespace=None,
+def custom_analyzer(text, namespace=None, attributes=None,
                     tokenizer='keyword', token_filter=[], char_filter=[],
-                    host='localhost', http_port=DEFAULT_HTTP_PORT):
+                    host='localhost', http_port=DEFAULT_HTTP_PORT,
+                    converter=None):
     if text is None:
         return []
     data = {"tokenizer": tokenizer,
             "filter": token_filter,
             "char_filter": char_filter,
             "text": text}
+    if attributes is not None:
+        data.update({"explain": True,
+                     "attributes": attributes})
+        if converter is None:
+            converter = lambda x: x
+    elif converter is None:
+        converter=default_converter
     url_suffix = '/_analyze' if namespace is None else '/' + namespace + '/_analyze'
     return send_analyze_request('http://' + host + ':' + str(http_port) + url_suffix,
-                                data)
+                                data,
+                                converter=converter)
 
 
-def send_analyze_request(url, data):
+def send_analyze_request(url, data, converter):
     req = Request(url)
     req.add_header('Content-Type', 'application/json')
     with closing(urlopen(req, json.dumps(data).encode('utf-8'))) as response:
         result = json.loads(response.read().decode('utf-8'))
-        return [x.get('token') for x in result.get('tokens')]
+        return converter(result)
--- tests/analyzers.py
+++ tests/analyzers.py
@@ -26,10 +26,40 @@ class AnalyzerTest(unittest.TestCase):
                                  analyzer='standard')
         self.assertEqual(result, ['this', 'is', 'a', 'pen'])
 
+    def test_analyzer_standard_explain(self):
+        def test_converter(result):
+            return [{"token": x.get('token'),
+                     "start_offset": x.get('start_offset'),
+                     "end_offset": x.get('end_offset'),
+                     "position": x.get('position'),
+                     "type": x.get('type'),
+                     "keyword": x.get('keyword')} for x in result.get('detail').get('analyzer').get('tokens')]
+        result = esanpy.analyzer('This is a pen.',
+                                 analyzer='standard',
+                                 attributes=['keyword'],
+                                 converter=test_converter)
+        self.assertEqual([x.get('token') for x in result], ['this', 'is', 'a', 'pen'])
+        self.assertEqual([x.get('start_offset') for x in result], [0, 5, 8, 10])
+        self.assertEqual([x.get('end_offset') for x in result], [4, 7, 9, 13])
+        self.assertEqual([x.get('position') for x in result], [0, 1, 2, 3])
+        self.assertEqual([x.get('type') for x in result], ['<ALPHANUM>', '<ALPHANUM>', '<ALPHANUM>', '<ALPHANUM>'])
+        self.assertEqual([x.get('keyword') for x in result], [None, None, None, None])
+
     def test_analyzer_kuromoji(self):
         result = esanpy.analyzer('今日の天気は晴れです。',
                                  analyzer='kuromoji')
         self.assertEqual(result, ['今日', '天気', '晴れ'])
+
+    def test_analyzer_kuromoji_explain(self):
+        def test_converter(result):
+            return [{"token": x.get('token'),
+                     "pos": x.get('partOfSpeech')} for x in result.get('detail').get('analyzer').get('tokens')]
+        result = esanpy.analyzer('今日の天気は晴れです。',
+                                 analyzer='kuromoji',
+                                 attributes=['partOfSpeech'],
+                                 converter=test_converter)
+        self.assertEqual([x.get('token') for x in result], ['今日', '天気', '晴れ'])
+        self.assertEqual([x.get('pos') for x in result], ['名詞-副詞可能', '名詞-一般', '名詞-一般'])
 
     def test_custom_analyzer_case1(self):
         result = esanpy.custom_analyzer('this is a <b>test</b>',
@@ -45,6 +75,27 @@ class AnalyzerTest(unittest.TestCase):
                                                       {"type": "stop", "stopwords": ["a", "is", "this"]}],
                                         char_filter=[])
         self.assertEqual(result, ['test'])
+
+    def test_custom_analyzer_case3(self):
+        result = esanpy.custom_analyzer('detailed output',
+                                        tokenizer="standard",
+                                        token_filter=["snowball"],
+                                        char_filter=[],
+                                        attributes=["keyword"])
+        tokens = result.get('detail').get('tokenizer').get('tokens')
+        self.assertEqual([x.get('token') for x in tokens], ['detailed', 'output'])
+        self.assertEqual([x.get('start_offset') for x in tokens], [0, 9])
+        self.assertEqual([x.get('end_offset') for x in tokens], [8, 15])
+        self.assertEqual([x.get('position') for x in tokens], [0, 1])
+        self.assertEqual([x.get('type') for x in tokens], ['<ALPHANUM>', '<ALPHANUM>'])
+        self.assertEqual([x.get('keyword') for x in tokens], [None, None])
+        tokens = result.get('detail').get('tokenfilters')[0].get('tokens')
+        self.assertEqual([x.get('token') for x in tokens], ['detail', 'output'])
+        self.assertEqual([x.get('start_offset') for x in tokens], [0, 9])
+        self.assertEqual([x.get('end_offset') for x in tokens], [8, 15])
+        self.assertEqual([x.get('position') for x in tokens], [0, 1])
+        self.assertEqual([x.get('type') for x in tokens], ['<ALPHANUM>', '<ALPHANUM>'])
+        self.assertEqual([x.get('keyword') for x in tokens], [False, False])
 
 
 if __name__ == "__main__":
